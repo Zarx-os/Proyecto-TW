0021a8442b6012fc4dfd480d83ac3c1d8da9d2e9
MauMM <amirelesm1500@gmail.com>
2021-05-25 18:09:11 -0500

    Siguiente

--- animaciones.js
+++ animaciones.js
@@ -5,20 +5,25 @@
 
       if (canvas.getContext) {
         var ctx = canvas.getContext("2d");
-
-
-        ctx.font = "0.8em Arial";
-        ctx.fillText(cadenas[0],canvas.width/2,canvas.height/2);
-        ctx.fillText(cadenas[1],canvas.width/2,(canvas.height/2)+15);
-        ctx.fillText(cadenas[2],canvas.width/2,(canvas.height/2)+30);
-        ctx.fillText(cadenas[3],canvas.width/2,(canvas.height/2)+45);
-        ctx.fillText(cadenas[4],canvas.width/2,(canvas.height/2)+60);
-        ctx.fillStyle = "rgb(200,0,0)";
-        ctx.fillRect (10, 10, 100, 100);
-
-        ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-        ctx.fillRect (30, 30, 55, 50);
       }
     }
 
-  window.addEventListener("load",draw,false);
+
+    function EstructPila(int dato){
+      let Pila= new Stack();
+    }
+
+
+    function mandarPagina(){
+      alert("Ahora");
+      console.log("Hla");
+    }
+
+
+    function cargar(){
+      let pagina=document.getElementById("pila");
+      pagina.addEventListener("click",mandarPagina,false);
+    }
+
+
+  window.addEventListener("load",cargar,false);
