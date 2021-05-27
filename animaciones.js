a8201e92893e309a165ef98bdbdf13d1ddaf7ea5
MauMM <amirelesm1500@gmail.com>
2021-05-27 00:05:07 -0500

    Empezamos de nuevo

--- animaciones.js
+++ animaciones.js
@@ -52,72 +52,31 @@ class Stack {
 
 }
 
-//Señorita Elizabethh
-function draw_arrow(context, startX, startY, size) {
-  var arrowX = startX + 0.75 * size;
-  var arrowTopY = startY - 0.707 * (0.25 * size);
-  var arrowBottomY = startY + 0.707 * (0.25 * size);
-  context.moveTo(startX, startY);
-  context.lineTo(startX + size, startX);
-  context.lineTo(arrowX, arrowTopY);
-  context.moveTo(startX + size, startX);
-  context.lineTo(arrowX, arrowBottomY);
-  context.stroke();
-}
+//--------------------------
+
+
 
 function mandarPagina() {
   alert("Ahora");
 }
 
+
+
+
+
+
+
+
+
+
+
+//Codigo para cargar el document
 function cargar() {
   let pagina = document.getElementById('pila');
   let canvas = document.getElementById('canvas');
   var boton = document.getElementById('insertarDato');
-  let ctx = canvas.getContext('2d');
-  ctx.lineWidth = 3;
-  draw_arrow(ctx, 25, 25, 40);
   pagina.addEventListener("click", mandarPagina, false);
   boton.addEventListener("click", appendColumn, false);
 }
-
-function agregartd() {
-  var celda = document.createElement("td");
-  var textoCelda = document.createTextNode("columna");
-  celda.appendChild(textoCelda);
-  hilera.appendChild(celda);
-}
-
-function appendRow() {
-  var tbl = document.getElementById('tablas'), // table reference
-    row = tbl.insertRow(tbl.rows.length); // append table row
-    createCell(row.insertCell(0),0, 'row'); //se refiere a lo que ira en el div
-}
-
-// create DIV element and append to the table cell
-function createCell(cell, text, style) {
-  var div = document.createElement('div'), // create DIV element
-    //let ctx1 =div.getContext('2d');z
-    txt = document.createTextNode(text); // create text node
-    let canvas1=document.createElement('canvas');
-    let ctx1 = canvas1.getContext('2d');
-    ctx1.lineWidth = 4;
-    draw_arrow(ctx1, 25, 25, 500);
-  div.appendChild(txt); // append text node to the DIV
-  div.setAttribute('class', style); // set DIV class attribute
-  div.setAttribute('className', style);
-  canvas1.setAttribute('class',style); // set DIV class attribute for IE (?!)
-  canvas1.setAttribute('className',style);
-  cell.appendChild(canvas1);
-  cell.appendChild(div); // append DIV to the table cell
-}
-
-
-
-function appendColumn() {
-  var tbl = document.getElementById('tablas'); // table reference
-  // open loop for each row and append cell
-    createCell(tbl.rows[0].insertCell(tbl.rows[0].cells.length), 0, 'canvas');
-}
-
 
 window.addEventListener("load", cargar, false);
